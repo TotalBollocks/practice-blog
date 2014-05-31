ee9d615e5b256dff5b6017eba0de22e410f02852
TotalBollocks <jqrainwater@yahoo.com>
2014-05-31 09:30:05 -0500

    Untag specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Article do
-  it "Has valid factory", focus: true do
+  it "Has valid factory" do
   	article = FactoryGirl.build(:article)
   	article.should be_valid
   end
 
-  it "requires subject", focus: true do
+  it "requires subject" do
   	article = FactoryGirl.build(:article, subject: nil)
   	article.should_not be_valid
   	article.errors[:subject].should include("can't be blank")
