2f545052d3cd7ccd0914008bdf073dd6eb296a07
Bollocks <jqrainwater@yahoo.com>
2014-06-04 19:36:24 -0800

    Fix section migration

--- db/migrate/20140530142746_create_sections.rb
+++ db/migrate/20140530142746_create_sections.rb
@@ -2,7 +2,7 @@ class CreateSections < ActiveRecord::Migration
   def change
     create_table :sections do |t|
     	t.text :content
-    	t.belongs_to :articles
+    	t.belongs_to :article
 
       t.timestamps
     end
