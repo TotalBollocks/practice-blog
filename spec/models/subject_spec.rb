a841623365b7bc8d4cf6024e737f6c88f15aadbb
Bollocks <jqrainwater@yahoo.com>
2014-06-06 22:12:57 -0800

    Reconfigure rspec

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe Subject do
 
-  it "requires name", focus: true do
+  it "requires name" do
   	subject = FactoryGirl.build :subject, name: ""
   	subject.should_not be_valid
   	subject.errors[:name].should include "can't be blank"
   end
 
-  it "has unique name", focus: true do
+  it "has unique name" do
   	existing = FactoryGirl.create :subject
   	subject = FactoryGirl.build :subject, name: existing.name
   	subject.should_not be_valid
   	subject.errors[:name].should include "has already been taken"
   end
 
-  it "can add an article", focus: true do
+  it "can add an article" do
   	subject = Subject.create name: "Fun"
   	article = Article.new title: "Pokemon"
   	subject.articles << article
