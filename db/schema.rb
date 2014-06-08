c07a1ef18d21aa233706159632b767cb6e277f15
Bollocks <jqrainwater@yahoo.com>
2014-06-08 16:44:21 -0500

    Add user model w/ secure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530142746) do
+ActiveRecord::Schema.define(version: 20140608214028) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140530142746) do
 
   create_table "subjects", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
