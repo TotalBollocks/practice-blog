ab757e4aa036f57900a815ea7f8fdbcd5b5dba1e
Bollocks <jqrainwater@yahoo.com>
2014-06-10 20:47:33 +0000

    Give seeded articles a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,7 @@ Subject.delete_all
 Article.delete_all
 Section.delete_all
 
-
-SUBJECTS.each do |subject|
-	s = Subject.find_or_create_by(name: subject) 
-
-	[1,2,3].each do |n|
-    a = Article.create! title: "#{s.name}-Article#{n}", subject: s
-
-		[1,2,3].each do |j|
-			Section.create! content: "Lorem doller set ipsum blah blah blah"*40, article: a
-		end
-	end
-end
-
-User.find_or_create_by(email: "admin123@example.com") do |a|
+author = User.find_or_create_by(email: "admin123@example.com") do |a|
   a.admin= true
   a.password = "secret"
   a.password_confirmation = "secret"
@@ -33,4 +20,16 @@ end
 User.find_or_create_by(email: "anon123@example.com") do |a|
   a.password = "secret"
   a.password_confirmation = "secret"
+end
+
+SUBJECTS.each do |subject|
+  s = Subject.create(name: subject) 
+
+	[1,2,3].each do |n|
+    a = Article.create! title: "#{s.name}-Article#{n}", subject: s, user: author
+
+		[1,2,3].each do |j|
+			Section.create! content: "Lorem doller set ipsum blah blah blah"*40, article: a
+		end
+	end
 end
