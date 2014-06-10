86e7068bfdc16db2eda93097474cc1dabff785a8
Bollocks <jqrainwater@yahoo.com>
2014-06-10 02:25:16 +0000

    Restrict admin controller to admins

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,11 @@
 class Admin::BaseController < ApplicationController
-
+  before_action :require_admin
+  
+  private
+  
+  def require_admin
+    unless current_user && current_user.admin
+      redirect_to root_path
+    end
+  end
 end
---
+++ spec/controllers/admin/base_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Admin::BaseController do
+  controller(Admin::BaseController) do
+    
+    def index
+      render text: "Success!"
+    end
+  end
+  
+  it "Rejects an anonymous user" do
+    user = FactoryGirl.create :user
+    session[:user_id] =  user.id
+    get "index"
+    response.should redirect_to root_path
+  end
+  
+  it "Allows an admin access" do
+    user = FactoryGirl.create :user, admin: true
+    session[:user_id] = user.id
+    get "index"
+    response.should be_success
+  end
+end
